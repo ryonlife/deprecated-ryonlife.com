965ecdf74220ef003774e39c6721ac2066bdb302
Ryan McKillen <ryan.mckillen@gmail.com>
2010-06-12 21:54:36 -0700

    caching

--- local/app.rb
+++ local/app.rb
@@ -1,18 +1,22 @@
-get "/" do
+def cache(content, opts={})
+  headers 'Cache-Control' => 'public, max-age=300'
+end
+
+get '/' do
   set_common_variables
   set_from_config(:title, :subtitle, :description, :keywords)
   @heading = @title
   @title = @title
   @articles = Page.find_articles[0..7]
-  @body_class = "home"
+  @body_class = 'home'
   cache haml(:index)
 end
 
-get "/blog" do
+get '/blog' do
   set_common_variables
   set_from_config(:title, :subtitle, :description, :keywords)
-  @heading = "Blog"
-  @title = "Blog"
+  @heading = 'Blog'
+  @title = 'Blog'
   @articles = Page.find_articles[0..999]
   cache haml(:blog)
 end
