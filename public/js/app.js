9df2c2fc386be48c7e9fd9f72468820f27136a6f
Ryan McKillen <ryan.mckillen@gmail.com>
2010-06-14 02:23:12 -0700

    code for background photos, overall design stuff, plus... I actually bloggedgit add .

--- public/js/app.js
+++ public/js/app.js
@@ -1,7 +1,33 @@
-$(function(){
+jQuery(function($){
   
-  $.backstretch("/attachments/backgrounds/index.jpg");
+  $("#view_grid").click(function(){
+    $this = $(this);
+    if($this.text().match(/view/)) {
+      $.fn.fontGrid(18, "rgba(255, 255, 255, 0.35)");
+      $(this).text("Ah, that's nice! Click again to turn it off.")
+    } else {
+      $("#grid").remove();
+      $this.text("Click here to view the baseline grid for this page.")
+    }
+    return false;
+  })
   
-  // $('a.blog').bubbletip($('#tip1_up'), { deltaDirection: 'down' });
+  $.fn.fontGrid = function(leading, color) {
+    $("body").append('<div id="grid">');
+    var $grid = $("#grid");
+    for(var i = 1; i < $(document).height() / leading; i++) {
+      $grid.append('<div class="baseline_grid_row">')
+    }
+    $grid.css({
+      position: "absolute",
+      top: 0,
+      zIndex: -1
+    })
+    .find(".baseline_grid_row")
+      .css({
+        height: leading - 1,
+        borderBottom: "1px solid "+color,
+      })
+  }
   
 });
